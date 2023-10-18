c97723ab7c7239e21a621b56df058e98b65fb8f3
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-10-18 18:42:04 +0530

    testing

--- instrumentation.js
+++ instrumentation.js
@@ -3,8 +3,8 @@ import tracker from '@middleware.io/agent-apm-nextjs';
 
 export function register() {
     tracker.track({
-        projectName: "nextjs-project-test3",
-        serviceName: "nextjs-service-test3",
+        projectName: "nextjs-project-test3.1",
+        serviceName: "nextjs-service-test3.1",
         accessToken: "xusuusalpvush63ud7zcg8bi3mauuptds528",
         target: "vercel",
     });
