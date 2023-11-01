413e67fb3543dfe0cfc18efc2452d753cf9f9bb5
Sanjay Suthar <sanjaymsuthar@gmail.com>
2023-11-01 12:09:59 +0530

    testing of Vercel flags in Bkoff

---
+++ instrumentation.ts
@@ -0,0 +1,24 @@
+// @ts-ignore
+import tracker from '@middleware.io/agent-apm-nextjs';
+
+export function register() {
+    /*tracker.track({
+        projectName: "nextjs-project-test3.1",
+        serviceName: "nextjs-service-test3.1",
+        accessToken: "xusuusalpvush63ud7zcg8bi3mauuptds528",
+        target: "vercel",
+    });*/
+    // @ts-ignore
+    tracker.track({
+        serviceName: "nextjs-vercel-test3.3",
+        accessToken: "owkqjdhzxsopbddfjbfqvnwetupehwjjgkmy",
+        target: "vercel",
+    });
+    /*tracker.track({
+        projectName: "vercel-project-71.2",
+        serviceName: "vercel-service-71.2",
+        accessToken: "xusuusalpvush63ud7zcg8bi3mauuptds528",
+        target: "https://p2i13hg.middleware.io:443",
+    });*/
+    tracker.warn("Deployment done successfully!");
+}
