5f132396e3c1588df55a32f256555662cc1c144e
Sanjay Suthar <sanjaymsuthar@gmail.com>
2024-02-05 15:01:47 +0530

    minor

--- instrumentation.ts
+++ instrumentation.ts
@@ -2,16 +2,10 @@
 import tracker from '@middleware.io/agent-apm-nextjs';
 
 export function register() {
-    /*tracker.track({
-        projectName: "nextjs-project-test3.1",
-        serviceName: "nextjs-service-test3.1",
-        accessToken: "xusuusalpvush63ud7zcg8bi3mauuptds528",
-        target: "vercel",
-    });*/
     // @ts-ignore
     tracker.track({
         serviceName: "nextjs-vercel-test3.1.0",
-        accessToken: "murwpaubimiwrfrtoxbwpoiuoztzdjgmsyph",
+        accessToken: "yzofakgtpcelqsexhzhegmticbrciftarhzd",
         target: "vercel",
     });
     /*tracker.track({
