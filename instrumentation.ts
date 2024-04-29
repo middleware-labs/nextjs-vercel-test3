8cede4dc3673b12db6623c23cb901fe82fec5495
Sanjay Suthar <sanjaymsuthar@gmail.com>
2024-04-29 14:09:29 +0530

    minor

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,13 +1,13 @@
 // @ts-ignore
-import tracker from '@middleware.io/agent-apm-nextjs';
+// import tracker from '@middleware.io/agent-apm-nextjs';
 
 export function register() {
 
-    tracker.track({
-        serviceName: "nextjs-vercel-test3",
-        accessToken: "murwpaubimiwrfrtoxbwpoiuoztzdjgmsyph",
-        target: "vercel",
-    });
-
-    tracker.warn("Deployment done successfully!!");
+    // tracker.track({
+    //     serviceName: "nextjs-vercel-test3",
+    //     accessToken: "murwpaubimiwrfrtoxbwpoiuoztzdjgmsyph",
+    //     target: "vercel",
+    // });
+    //
+    // tracker.warn("Deployment done successfully!!");
 }
