309dbf9588ffcb2c4e0dc6bcfd6a6bd26c718b03
Hardik Choksi <hardik@mw.io>
2025-08-01 18:48:41 +0530

    Add error handling in API endpoint for people; throw an error if query parameter 'error' is set to 'true'.

--- pages/api/people/index.ts
+++ pages/api/people/index.ts
@@ -6,5 +6,10 @@ export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<Person[]>
 ) {
+  // if there exists a query parameter called 'error' and it is equal to 'true', throw an error
+  const error = _req.query.error;
+  if (error === 'true') {
+    throw new Error('This is a new error for c');
+  }
   return res.status(200).json(people)
 }
