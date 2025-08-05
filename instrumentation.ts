f0810ce95482414bf731223f3b2e850f5ccce258
Hardik Choksi <hardik@mw.io>
2025-08-05 14:55:45 +0530

    Refactor instrumentation and update dependencies; replace tracker.track with registerOTel for improved telemetry integration, and update package versions for OpenTelemetry libraries in package.json and package-lock.json.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,17 +1,21 @@
 // @ts-ignore
-import tracker from '@middleware.io/agent-apm-nextjs';
+import tracker from "@middleware.io/agent-apm-nextjs";
+import { registerOTel } from "@vercel/otel";
 
 export function register() {
+  // tracker.track({
+  //     serviceName: "nextjs-vercel-test3",
+  //     accessToken: "lneevcqdcnzccpdpbhelkxpjffdxeznkryvb",
+  //     // accessToken: "ohpv5ncdrwxjlr28xce4a9tmi3fvdcopn3f5",
+  //     // target: "vercel",
+  //     customResourceAttributes: {
+  //         "app.version": "2.0.0"
+  //     }
+  // });
 
-    tracker.track({
-        serviceName: "nextjs-vercel-test3",
-        accessToken: "lneevcqdcnzccpdpbhelkxpjffdxeznkryvb",
-        // accessToken: "ohpv5ncdrwxjlr28xce4a9tmi3fvdcopn3f5",
-        target: "vercel",
-        customResourceAttributes: {
-            "app.version": "2.0.0"
-        }
-    });
+  registerOTel({
+    serviceName: "nextjs-vercel-test3",
+  });
 
-    tracker.warn("Deployment done successfully!");
+  tracker.warn("Deployment done successfully!");
 }
--- next.config.js
+++ next.config.js
@@ -8,14 +8,14 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@middleware.io/agent-apm-nextjs"],
   },
   webpack: (config, { isServer }) => {
-    config.plugins.push(
-      new MiddlewareWebpackPlugin(
-        "lneevcqdcnzccpdpbhelkxpjffdxeznkryvb", // Account key of the application.
-        "2.0.0", // Application verison
-        ".next/", // By default path of next.js where sourcemap resides.
-        ".next/" // Base path where your sourcemap will reside after upload.
-      )
-    );
+    // config.plugins.push(
+    //   new MiddlewareWebpackPlugin(
+    //     "lneevcqdcnzccpdpbhelkxpjffdxeznkryvb", // Account key of the application.
+    //     "2.0.0", // Application verison
+    //     ".next/", // By default path of next.js where sourcemap resides.
+    //     ".next/" // Base path where your sourcemap will reside after upload.
+    //   )
+    // );
 
     if (isServer) {
       config.devtool = "source-map";
